54848545138b90c587ce7ecf7f7f5b89e3ab60c5
Tung Pham <tung.42@gmail.com>
2026-03-26 17:49:17 +0700

    Update landing.blade.php

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -13,6 +13,12 @@
     <!-- Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
 
+    <script>
+        tailwind.config = {
+            darkMode: 'class',
+        }
+    </script>
+
     <!-- Alpine.js -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
